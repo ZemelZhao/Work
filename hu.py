548cbc28915f3ba5a49c0f74c307d94a5d3d5eb0
ZemelZhao <zemzhao@163.com>
2018-12-03 23:03:34 +0800

    td_all

--- hu.py
+++ hu.py
@@ -15,22 +15,24 @@ def td(data):
     cache.append(np.sum((data[:-2] - data[1:-1])*(data[1:-1] - data[2:])>=0))
     return np.array(cache)
 
-data_file = os.path.join('emgdata', '0.csv')
+for _ in [0, 1, 3, 4]:
 
-for i in range(0, 7, 2):
-    data_cache = []
-    reader = csv.reader(open(data_file))
-    for row in reader:
-        data_cache.append(float(row[i]))
-    data_cache = data_cache[1000: -1000]
-    feat_td_temp = np.array([0, 0, 0, 0])
-    for j in range(0, 2900, 100):
-        data_temp = data_cache[j : j + 100]
-        feat_td_temp = np.vstack((feat_td_temp, td(data_temp)))
-    try:
-        feat_td = np.hstack((feat_td, feat_td_temp))
-    except:
-        feat_td = feat_td_temp
+    data_file = os.path.join('emgdata', '%i.csv' %_)
 
-np.save('feat_td0.npy', feat_td)
+    for i in range(0, 7, 2):
+        data_cache = []
+        reader = csv.reader(open(data_file))
+        for row in reader:
+            data_cache.append(float(row[i]))
+        data_cache = data_cache[1000: -1000]
+        feat_td_temp = np.array([0, 0, 0, 0])
+        for j in range(0, 2900, 100):
+            data_temp = data_cache[j : j + 100]
+            feat_td_temp = np.vstack((feat_td_temp, td(data_temp)))
+        try:
+            feat_td = np.hstack((feat_td, feat_td_temp))
+        except:
+            feat_td = feat_td_temp
+
+    np.save('feat_td%i.npy' % _, feat_td)
 
